80eb21c27989a0c4a2e5a633c7ebb13f9c622d9f
yugalrajput <118297721+yugalrajput@users.noreply.github.com>
2024-02-11 23:27:39 +0530

    ok

--- src/JDBC/StoredProcedureCountOUT.java
+++ src/JDBC/StoredProcedureCountOUT.java
@@ -4,18 +4,16 @@ import java.sql.*;
 
 public class StoredProcedureCountOUT {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/employetable","root","root");
-            CallableStatement cs=con.prepareCall("{call countemployee(?)}");
-            System.out.println("hi");
-            cs.registerOutParameter(1,Types.INTEGER);
-
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employetable", "root", "root");
+            CallableStatement cs = con.prepareCall("{call countemployee(?)}");
+            cs.registerOutParameter(1, Types.INTEGER);
             cs.execute();
-            System.out.println("hi");
-            int count=cs.getInt(1);
-            System.out.println("success" + " "+count);
+
+            int count = cs.getInt(1);
+            System.out.println("success" + " " + count);
 
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
